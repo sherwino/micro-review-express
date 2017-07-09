ea56bc73c72e7ea3b4187c31d8d14ccd783dd7d8
Sherwin Garcia-Obregon <sherwination@gmail.com>
2017-07-09 01:00:06 -0400

    Fixed front page

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // default value for title local
-app.locals.title = 'Project-Man';
+app.locals.title = 'microReviews';
 //
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -43,6 +43,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, './lib/')));
 app.use(layouts);
 app.use(session({
   key: "user-session",
